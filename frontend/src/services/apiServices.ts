5b091ec3ca4cdac0b42d93380a9e15a61e43cd1b
Vedant bhavsar <vedantbhavsar.a10@gmail.com>
2024-10-31 09:20:16 +0530

    deployment

--- frontend/src/services/apiServices.ts
+++ frontend/src/services/apiServices.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const authToken = localStorage.getItem("token") || "";
 const axiosApi = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: process.env.API_URL || "http://localhost:3001",
   headers: {
     Authorization: authToken ? `Bearer ${authToken}` : "",
   },
